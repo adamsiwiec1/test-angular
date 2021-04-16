03844c227198da5ebc9f8a7c09f3b1caa617cbe7
adamsiwiec1 <73085927+adamsiwiec1@users.noreply.github.com>
2021-04-16 00:03:31 -0400

    vscode is dumb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+  
+  <!-- header --> 
+  <div class="navbar is-dark">
+    <!-- logo --> 
+    <div class="navbar-brand">
+      <a class="navbar-item">
+        SiwiecTech
+      </a>
+    </div>
+  </div>
+
+  <!-- routes get injected here -->
+  <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
   title = 'test-angular';
